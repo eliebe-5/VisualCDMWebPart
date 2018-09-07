c7ac6fc24d5456f2ae25cc0305d63f011194313a
Elias Eberhardsson <Elias.Eberhardsson@dataductus.se>
2018-09-07 13:57:49 +0200

    Initial setup

---
+++ src/webparts/cdmPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICdmPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CdmPartWebPartStrings' {
+  const strings: ICdmPartWebPartStrings;
+  export = strings;
+}
